36546dbc96d9679e920e90dac7b18ddfe7360028
Jitendra Kumar <Jitendra.devops.eng@gmail.com>
2026-06-30 06:43:41 +0530

    fixed error

--- Future-x_Web/src/main.tsx
+++ Future-x_Web/src/main.tsx
@@ -1,15 +1,18 @@
+/** @jsxRuntime classic */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 
+// Suppress TypeScript error for side-effect CSS import when no type declarations are present
+// @ts-ignore: Cannot find module or type declarations for side-effect import of './index.css'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/Future-x_Website">
         <App />
       </BrowserRouter>
     </HelmetProvider>
