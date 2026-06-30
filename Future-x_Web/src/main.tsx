1e3d08f7ab8d2b3207c36b988286f808cfa463d8
Jitendra Kumar <Jitendra.devops.eng@gmail.com>
2026-06-30 08:00:25 +0530

    fixed error

--- Future-x_Web/src/main.tsx
+++ Future-x_Web/src/main.tsx
@@ -12,7 +12,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
